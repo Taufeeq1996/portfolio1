6918b6b8ad7cfef3038884448c1ebcbfb8657f3c
Taufeeq1996 <taufeeqk@techis.io>
2024-05-02 15:23:18 +0530

    completed nav

---
+++ index.js
@@ -0,0 +1,5 @@
+// Get the viewport width
+var viewportWidth = window.innerWidth || document.documentElement.clientWidth;
+
+// Log the viewport width to the console
+console.log("Viewport width: " + viewportWidth + "px");
