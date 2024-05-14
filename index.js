77be34c48fb2ec98d1a256df74aeaec80a9eb7cc
Taufeeq1996 <taufeeqk@techis.io>
2024-05-14 11:31:18 +0530

    removed js file

--- index.js
+++
@@ -1,5 +0,0 @@
-// Get the viewport width
-var viewportWidth = window.innerWidth || document.documentElement.clientWidth;
-
-// Log the viewport width to the console
-console.log("Viewport width: " + viewportWidth + "px");
